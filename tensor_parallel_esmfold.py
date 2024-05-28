37c712bafcb18950f16e354e5d6c401fdaa417c7
awer-A <2568117980@qq.com>
2024-05-28 16:15:50 +0800

    Implementing AutoTensor Parallel through DeepSpeed

--- tensor_parallel_esmfold.py
+++ tensor_parallel_esmfold.py
@@ -4,6 +4,7 @@ import transformers
 import deepspeed
 from transformers.models.t5.modeling_t5 import T5Block
 import torch.distributed as dist
+from transformers import AutoTokenizer, EsmForProteinFolding
 '''
 export RANK=0
 export WORLD_SIZE=4
@@ -13,12 +14,24 @@ export MASTER_PORT=12355
 dist.init_process_group(backend='nccl')
 local_rank = int(os.getenv("LOCAL_RANK", "0"))
 world_size = int(os.getenv("WORLD_SIZE", "4"))
+# Load model directly
+tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
+model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1")
 
-# create the model pipeline
-pipe = transformers.pipeline("text-classification", model="facebook/esmfold_v1", device=local_rank)
 # Initialize the DeepSpeed-Inference engine
-pipe.model = deepspeed.init_inference(
-    pipe.model,
+model.esm = deepspeed.init_inference(
+    model.esm,
     mp_size=world_size,
     dtype=torch.float,
 )
+
+sequence = 'MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVLGYNIVATPRGYVLAGG'
+
+inputs = tokenizer(sequence , return_tensor="pt", add_special_tokens=False)
+device = torch.device('cuda' if  torch.cuda.is_avaible() else 'cpu')
+model = model.to(device)
+inputs = {k:v.to(device) for k,v in inputs.items()}
+
+output = model(**inputs)
+
+print(output)
